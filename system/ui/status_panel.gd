2f8ae05a5b713cae4b07c85ea85da2eeda98ea83
Vegt7day <2990808366@qq.com>
2026-04-19 18:19:38 +0800

    调整了血量ui

--- system/ui/status_panel.gd
+++ system/ui/status_panel.gd
@@ -1,8 +1,9 @@
-extends HBoxContainer
+extends Node2D
 
 @export var stats:Stats
 
-@onready var health_bar:TextureProgressBar = $HealthBar
+
+@onready var health_bar: TextureProgressBar = $"status panel/HealthBar"
 
 func _ready() -> void:
 	stats.health_changed.connect(update_heath)
